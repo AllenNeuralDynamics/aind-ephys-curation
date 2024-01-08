e75812d9d9dc19eb4d87d9cf845b156628ddca81
Alessio Buccino <alejoe9187@gmail.com>
2024-01-08 13:06:16 +0100

    Update run and remove postInstall

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,9 +1,9 @@
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # GENERAL IMPORTS
 import os
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 import numpy as np
 from pathlib import Path
@@ -15,11 +15,9 @@ import spikeinterface as si
 import spikeinterface.qualitymetrics as sqm
 import spikeinterface.curation as sc
 
-from spikeinterface.core.core_tools import check_json
 
 # AIND
-from aind_data_schema import Processing
-from aind_data_schema.processing import DataProcess
+from aind_data_schema.core.processing import DataProcess
 
 
 URL = "https://github.com/AllenNeuralDynamics/aind-capsule-ephys-curation"
@@ -27,17 +25,13 @@ VERSION = "0.1.0"
 
 
 curation_params = dict(
-        isi_violations_ratio_threshold=0.5,
-        presence_ratio_threshold=0.8,
-        amplitude_cutoff_threshold=0.1,
-    )
+    isi_violations_ratio_threshold=0.5,
+    presence_ratio_threshold=0.8,
+    amplitude_cutoff_threshold=0.1,
+)
 
 
-job_kwargs = {
-    'n_jobs': -1,
-    'chunk_duration': '1s',
-    'progress_bar': True
-}
+job_kwargs = {"n_jobs": -1, "chunk_duration": "1s", "progress_bar": True}
 
 data_folder = Path("../data/")
 scratc_folder = Path("../scratch")
@@ -46,7 +40,7 @@ results_folder = Path("../results/")
 
 if __name__ == "__main__":
     data_process_prefix = "data_process_curation"
-    
+
     si.set_global_job_kwargs(**job_kwargs)
 
     ####### CURATION ########
@@ -60,14 +54,14 @@ if __name__ == "__main__":
     presence_ratio_thr = curation_params["presence_ratio_threshold"]
     amplitude_cutoff_thr = curation_params["amplitude_cutoff_threshold"]
 
-    
-
     # check if test
     if (data_folder / "postprocessing_pipeline_output_test").is_dir():
         print("\n*******************\n**** TEST MODE ****\n*******************\n")
         postprocessed_folder = data_folder / "postprocessing_pipeline_output_test"
-        
-        curation_query = f"isi_violations_ratio < {isi_violations_ratio_thr} and amplitude_cutoff < {amplitude_cutoff_thr}"
+
+        curation_query = (
+            f"isi_violations_ratio < {isi_violations_ratio_thr} and amplitude_cutoff < {amplitude_cutoff_thr}"
+        )
         del curation_params["presence_ratio_threshold"]
     else:
         curation_query = f"isi_violations_ratio < {isi_violations_ratio_thr} and presence_ratio > {presence_ratio_thr} and amplitude_cutoff < {amplitude_cutoff_thr}"
@@ -76,7 +70,9 @@ if __name__ == "__main__":
     print(f"Curation query: {curation_query}")
     curation_notes += f"Curation query: {curation_query}\n"
 
-    postprocessed_folders = [p for p in postprocessed_folder.iterdir() if "postprocessed_" in p.name and "-sorting" not in p.name]
+    postprocessed_folders = [
+        p for p in postprocessed_folder.iterdir() if "postprocessed_" in p.name and "-sorting" not in p.name
+    ]
     for postprocessed_folder in postprocessed_folders:
         datetime_start_curation = datetime.now()
         t_curation_start = time.perf_counter()
@@ -107,26 +103,22 @@ if __name__ == "__main__":
 
         # save params in output
         curation_params["recording_name"] = recording_name
-        
-        curation_outputs = dict(
-            total_units=n_units,
-            passing_qc=n_passing,
-            failing_qc=n_units - n_passing
-        )
+
+        curation_outputs = dict(total_units=n_units, passing_qc=n_passing, failing_qc=n_units - n_passing)
         curation_process = DataProcess(
-                name="Ephys curation",
-                version=VERSION, # either release or git commit
-                start_date_time=datetime_start_curation,
-                end_date_time=datetime_start_curation + timedelta(seconds=np.floor(elapsed_time_curation)),
-                input_location=str(data_folder),
-                output_location=str(results_folder),
-                code_url=URL,
-                parameters=curation_params,
-                outputs=curation_outputs,
-                notes=curation_notes
-            )
+            name="Ephys curation",
+            software_version=VERSION,  # either release or git commit
+            start_date_time=datetime_start_curation,
+            end_date_time=datetime_start_curation + timedelta(seconds=np.floor(elapsed_time_curation)),
+            input_location=str(data_folder),
+            output_location=str(results_folder),
+            code_url=URL,
+            parameters=curation_params,
+            outputs=curation_outputs,
+            notes=curation_notes,
+        )
         with open(curation_output_process_json, "w") as f:
-            f.write(curation_process.json(indent=3))
+            f.write(curation_process.model_dump_json(indent=3))
 
     t_curation_end_all = time.perf_counter()
     elapsed_time_curation_all = np.round(t_curation_end_all - t_curation_start_all, 2)
