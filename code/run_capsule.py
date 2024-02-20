3453bfe103f0ec686b1499ce9d578ce1c34cc437
Alessio Buccino <alessio.buccino@alleninstitute.org>
2024-02-20 15:11:19 +0000

    Added metadata and v1.0

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -21,7 +21,7 @@ import spikeinterface.curation as sc
 from aind_data_schema.core.processing import DataProcess
 
 URL = "https://github.com/AllenNeuralDynamics/aind-ephys-curation"
-VERSION = "0.1.0"
+VERSION = "1.0"
 
 data_folder = Path("../data/")
 scratch_folder = Path("../scratch")
